41dd43b818a0f5cb903647855e84defb7e6ebe8c
David Whitney <codifyamplify@gmail.com>
2023-02-18 13:47:56 -0500

    generated create_exercises create table and attributes content

--- db/migrate/20230218184246_create_exercises.rb
+++ db/migrate/20230218184246_create_exercises.rb
@@ -1,4 +1,20 @@
 class CreateExercises < ActiveRecord::Migration[6.1]
   def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :muscle_group
+      t.integer :weight
+      t.integer :sets
+      t.integer :reps
+      t.string :rest
+    end
   end
 end
+
+
+# name
+# muscle_group
+# weight
+# sets
+# reps
+# rest
