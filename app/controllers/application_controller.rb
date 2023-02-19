601e3ccff4be13851bae0e77f6523652846b46f4
David Whitney <codifyamplify@gmail.com>
2023-02-18 20:47:42 -0500

    generating CREATE route for User model and CREATE/READ routes for Exercise model in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,10 +24,40 @@ class ApplicationController < Sinatra::Base
     user.to_json
   end
   # build CREATE route - POST new user, (.create)
+  post '/users' do
+    user = User.create(
+      name: params[:name]
+    )
+    user.to_json
+  end
+
 
 # EXERCISE ROUTES
   # build READ route - GET all exercises (.all), get a specific exercise (.find)
+  get '/exercises' do
+    exercises = Exercise.all
+    exercises.to_json
+  end
+
+  get '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    exercise.to_json
+  end
+
   # build CREATE route - POST new exercise, (.create)
+  post '/exercises' do
+    exercise = Exercise.create(
+      name: params[:name],
+      muscle_group: params[:muscle_group],
+      weight: params[:weight],
+      sets: params[:sets],
+      reps: params[:reps],
+      rest: params[:rest],
+      user_id: params[:user_id]
+    )
+    exercise.to_json
+  end
+
   # build DELETE route - DELETE specific route, (.find, .destroy)
   # build UPDATE route - PATCH specific route, (.update)
 
