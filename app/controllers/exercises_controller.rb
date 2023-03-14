bde208e470acffe104d782a817a6eca7b87df92f
David Whitney <codifyamplify@gmail.com>
2023-03-14 19:43:14 -0400

    removed strng params, wrote by hand in routes

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,12 +8,26 @@ class ExercisesController < ApplicationController
 
   get '/exercises/:id' do
     exercise = find_exercise
-    exercise.to_json(include: [:user])
+    # add conditional - if exercise exists, else
+    # if exercise
+      exercise.to_json(include: [:user])
+    # else
+    #   { message: "Sorry, that exercise was not found."}.to_json
+    # end
   end
 
   # build CREATE route - POST new exercise, (.create)
   post '/exercises' do
-    exercise = Exercise.create(exercise_params)
+    # binding.pry
+    exercise = Exercise.create(
+      name: params[:name],
+      muscle_group: params[:muscle_group],
+      weight: params[:weight],
+      sets: params[:sets],
+      reps: params[:reps],
+      rest: params[:rest],
+      user_id: params[:user_id]
+    )
     exercise.to_json(include: [:user])
   end
 
@@ -26,9 +40,20 @@ class ExercisesController < ApplicationController
 
   # build UPDATE route - PATCH specific route, (.update)
   patch '/exercises/:id' do
-    exercise = find_exercise
-    exercise.update(exercise_params)
+    # binding.pry
+    exercise = Exercise.find(params[:id])
+    # binding.pry
+    exercise.update(
+      name: params[:updatedName],
+      muscle_group: params[:updatedMuscleGroup],
+      weight: params[:updatedWeight],
+      sets: params[:updatedSets],
+      reps: params[:updatedReps],
+      rest: params[:updatedRest],
+      user_id: params[:updatedUserId]
+    )
     exercise.to_json(include: [:user])
+    # binding.pry
   end
 
   private
@@ -36,13 +61,13 @@ class ExercisesController < ApplicationController
   # we use this method to create a list of what's permitted to be passed to .create or .update
   # within controller actions.
 #   strong params
-  def exercise_params
-    allowed_params = %w(name muscle_group weight sets reps rest user_id)
-    params.select{|k,v| allowed_params.include?(k)}
-  end
+  # def exercise_params
+  #   allowed_params = %w(name muscle_group weight sets reps rest user_id)
+  #   params.select{|k,v| allowed_params.include?(k)}
+  # end
 
   def find_exercise
-    Exercise.find(params[:id])
+    exercise = Exercise.find(params[:id])
   end
 
 end
